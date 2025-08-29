491373a3ed370b974cffb577b9702b343d406b42
Robert Badgett <bob.badgett@gmail.com>
2025-08-28 21:45:30 -0500

    added section about data sources not used

--- app.R
+++ app.R
@@ -101,49 +101,90 @@ ui <- page_sidebar(
           "Party labels colored: Democrat (blue), Republican (red), Other (gray). ",
           strong("Outliers are highlighted in light pink."))
     ),
-    ### Tab 4: About / Sources / Help -----
+    ##* Tab 4: About / Sources / Help -----
     tabPanel(
       title = "About / Sources / Help",
       h3("About / Sources / Help"),
       ###* Data sources ----
       h4("Data sources:"),
       tags$div("FEC.gov"),
-              tags$div(
-                tags$ul(
-                  tags$li(
-                  tags$a(href = "https://api.open.fec.gov/developers/", "open.FEC developers", target = "_blank")
-                ),
-                  tags$li(
-                    tags$a(href = "https://www.fec.gov/data/receipts/", "Receipts", target = "_blank")
-                  )),
-                tags$li(
-                  tags$a(href = "https://www.fec.gov/files/bulk-downloads/2026/ccl26.zip", "Crosswalk: candidate-committee", target = "_blank")
-              ),
-              tags$div("VoteView.gov"),
-                      tags$ul(
-                        tags$li(
-                          tags$a(href = "https://voteview.com/data", "Data", target = "_blank")
-                        )
-                      ),
-              tags$div("GOVTRACK.us"),
-                      tags$ul(
-                        tags$li(
-                          tags$a(href = "https://www.govtrack.us/congress/members/current", "Congressional members list", target = "_blank")
-                        ),
-                        tags$li(
-                          tags$a(href = "https://www.govtrack.us/accounts/lists", "Create tracker", target = "_blank")
-                        )
-              ),
-              tags$div("opensecrets.org"),
-                   tags$ul(
-                     tags$li(
-                       tags$a(href = "https://www.opensecrets.org/members-of-congress/members-list", "Congressional members list", target = "_blank")
-                     ),
-                     tags$li(
-                       tags$a(href = "https://www.opensecrets.org/bulk-data", "Data", target = "_blank")
-                     )
-                ),
-              tags$h4("Help:"),
+        tags$ul(
+          tags$li(
+            tags$a(href = "https://api.open.fec.gov/developers/", "open.FEC developers", target = "_blank")
+          ),
+          tags$li(
+            tags$a(href = "https://www.fec.gov/data/receipts/", "Receipts", target = "_blank")
+          )),
+        tags$li(
+          tags$a(href = "https://www.fec.gov/files/bulk-downloads/2026/ccl26.zip", "Crosswalk: candidate-committee", target = "_blank")
+        ),
+      tags$li(
+        tags$a(href = "https://fecnotify.fec.gov/fecnotify/register/", "Create tracker", target = "_blank")
+      ),
+        tags$div("VoteView.gov"),
+        tags$ul(
+          tags$li(
+            tags$a(href = "https://voteview.com/data", "Data", target = "_blank")
+          )
+        ),
+        tags$div("GOVTRACK.us"),
+        tags$ul(
+          tags$li(
+            tags$a(href = "https://www.govtrack.us/congress/members/current", "Congressional members list", target = "_blank")
+          ),
+          tags$li(
+            tags$a(href = "https://www.govtrack.us/accounts/lists", "Create tracker", target = "_blank")
+          )
+        ),
+        tags$div("opensecrets.org"),
+        tags$ul(
+          tags$li(
+            tags$a(href = "https://www.opensecrets.org/members-of-congress/members-list", "Congressional members list", target = "_blank")
+          ),
+          tags$li(
+            tags$a(href = "https://www.opensecrets.org/bulk-data", "Data", target = "_blank")
+          )
+        ),
+        ###* Finance sources *not* used ----
+      h4("Data sources not used:"),
+      tags$div("These entities cannot earmark/designate money from companies to candidates."),
+      tags$ul(
+        tags$li("Party national committees. The two major parties each have three national parties: their National Committee, Senatorial Campaign Committee, and Congressional Campaign Committee (",
+          tags$a(href = "https://ballotpedia.org/Party_committee_fundraising,_2025-2026", "Ballotopedia", target = "_blank"), ") However national committees do not earmark money from sources to candidates."
+        ),
+        tags$li(tags$a(href = "https://www.fec.gov/help-candidates-and-committees/joint-fundraising-candidates-political-committees/", "Conduit Committees", target = "_blank"),"do earmark.designate money to candidates, but only from contributions by individual citizens.",
+          # Conduit committees: ActBlue and WinRed
+            tags$ul(
+              tags$li(tags$a(href = "https://www.fec.gov/help-candidates-and-committees/joint-fundraising-candidates-political-committees/", "ActBlue", target = "_blank")),
+              tags$li(tags$a(href = "https://www.fec.gov/help-candidates-and-committees/joint-fundraising-candidates-political-committees/", "WinRed", target = "_blank"))
+              )
+            ),
+        tags$li( "Joint fund raising committees (JFCs) (",
+          tags$a(href = "https://www.fec.gov/help-candidates-and-committees/joint-fundraising-candidates-political-committees/", "FEC.gov", target = "_blank"), ", ",
+          tags$a(href = "https://www.opensecrets.org/joint-fundraising-committees-jfcs", "OpenSecrets.org", target = "_blank",")")
+        )
+      ),
+    ),
+    # Help
+    tags$h4("Help:"),
+    tags$ul(
+      tags$li(
+        tags$a(href = "https://github.com/badgettrg/SumSearch/issues", "Submit an issue at GitHub (requires a Google or GitHub account)", target = "_blank")
+      )
+    ),
+    
+      ###* About ----
+      h4("About:"),
+      div(HTML(
+        'The first SUMSearch was a medical meta-search engine started as "Medical SmartSearch" in October, 1998 
+   (Wayback archive <a href="https://web.archive.org/web/20010608194559/http://sumsearch.uthscsa.edu/searchform5.htm">2000</a> 
+   and <a href="https://web.archive.org/web/20100916215413/http://sumsearch.org/">2010</a>.)')),
+      div(HTML("It was well-reviewed in its day (PMID <a href=\'https://pubmed.gov/\'>17603909</a>; <a href=\'https://pmc.ncbi.nlm.nih.gov/articles/PMC2000788/\'>PMC2000788</a>) but as continously and evidence-linked resources for healthcare practioners emerged like UpToDate and Dynamed, SUMSearch was retired in 2024. Lets hope that difficulties with money in politics also wanes.")
+      ))
+  )
+    ),
+      # Help
+      tags$h4("Help:"),
                       tags$ul(
                         tags$li(
                           tags$a(href = "https://github.com/badgettrg/SumSearch/issues", "Submit an issue at GitHub (requires a Google or GitHub account)", target = "_blank")
